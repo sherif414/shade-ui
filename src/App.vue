72fbd1b2b64ecf0a7ba6450c3da427e8ed498372
shareef <creazyhell@gmail.com>
2023-02-12 18:09:05 +0200

    skip

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,24 @@
 <script setup lang="ts">
-const options = []
+import { useToast } from './composables/toast'
 
-for (let i = 0; i < 1000; i++) {
+const options: { label: string; value: string }[] = []
+
+for (let i = 0; i < 10; i++) {
   options.push({
     label: 'option ' + i,
     value: `${i}`,
   })
 }
-
-const v = ref()
-const loading = ref(false)
+const bool = ref(false)
+const v = ref('value')
 </script>
 
 <template>
   <main class="grid gap-4 place-items-center bg-light-1 min-h-screen">
     <form @submit.prevent="" class="flex flex-col rounded-3 p-4 pt-6 gap-8 max-w-300px">
       {{ v }}
-      <SInput label="username" />
-      <SSelect v-model="v" :options="options" label="select" />
-      <div class="row gap-2">
-        <SInput type="number" min="1" max="12" label="day" />
-        -<SInput type="number" min="1" max="12" label="month" />-
-        <SInput type="number" min="1" max="12" label="year" />
-      </div>
-      <div class="gap-4 fcc justify-between">
-        <SButton variant="text">button</SButton>
-        <SButton disabled :loading="loading" variant="text">
-          <template #icon>
-            <IconUser />
-          </template>
-        </SButton>
-        <SButton disabled :loading="loading" variant="default">
-          <template #icon>
-            <IconUser />
-          </template>
-        </SButton>
-        <SButton @click="loading = !loading">button</SButton>
-      </div>
+      <SInput v-model="v" :readonly="bool" hint="this is a hint" label="username"> </SInput>
+      <SButton @click="bool = !bool">button</SButton>
     </form>
   </main>
 </template>
