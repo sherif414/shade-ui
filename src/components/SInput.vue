576fe55c53eda14f1473d3d4460ed82df00dc661
shareef <creazyhell@gmail.com>
2023-02-12 17:47:36 +0200

    refactor input

--- src/components/SInput.vue
+++ src/components/SInput.vue
@@ -1,58 +1,72 @@
 <script setup lang="ts">
-const props = withDefaults(
-  defineProps<{
-    label?: string
-    modelValue?: string
-    type?: 'text' | 'email' | 'password' | 'number' | 'tel' | 'time' | 'url'
-    modelModifiers?: { lazy?: boolean }
-    class?: string
-    error?: boolean
-    errorMessage?: string
-  }>(),
-  {
-    type: 'text',
-    label: undefined,
-    modelValue: undefined,
-    modelModifiers: () => ({}),
-    class: '',
-    errorMessage: undefined,
-  }
-)
+export interface Props {
+  label?: string
+  modelValue?: string
+  type?: 'text' | 'email' | 'password' | 'number' | 'tel' | 'time' | 'url'
+  modelModifiers?: { lazy?: boolean }
+  disabled?: boolean
+  readonly?: boolean
+  error?: boolean
+  errorMessage?: string
+  hint?: string
+  class?: string
+}
 
-defineEmits(['update:modelValue'])
-const fallthroughClasses = computed(() => props.class)
-const eventType = computed(() => (props.modelModifiers.lazy ? 'change' : 'input'))
+// 👉 props, attrs, slots
+
+const props = withDefaults(defineProps<Props>(), {
+  type: 'text',
+})
+const { class: _, ...fallThroughAttrs } = useAttrs()
+const slots = useSlots()
+
+// 👉 computed classes
+
+const labelClasses = computed(() => {
+  if (slots.icon) {
+    return 'peer-not-placeholder-shown:(floating-label translate-x--10) peer-focus:(floating-label translate-x--10) left-10'
+  } else {
+    return 'peer-not-placeholder-shown:floating-label peer-focus:floating-label left-4'
+  }
+})
+
+// 👉 input attributes
+// ❕ vue doesn't provide .lazy modifier for custom component v-model
+const eventType = computed(() => (props.modelModifiers?.lazy ? 'change' : 'input'))
+const inputId = `${props.label}-${Math.round(Math.random() * 1000)}`
 </script>
 
 <template>
-  <label :class="[fallthroughClasses]" class="flex items-center relative h-10">
-    <div v-if="$slots.icon" class="absolute w-10 h-full fcc">
-      <slot name="icon"></slot>
+  <div :class="[props.error && 'text-red-400!', props.class, 'column items-start gap-1']">
+    <div class="h-10 relative w-full row justify-start">
+      <!-- 👉 appended icon -->
+      <div v-if="$slots.icon" class="absolute w-10 h-full center left-0">
+        <slot name="icon"></slot>
+      </div>
+
+      <!-- 👉 input -->
+      <input
+        ref="inputEl"
+        v-bind="fallThroughAttrs"
+        placeholder=" "
+        @[eventType]="$emit('update:modelValue', ($event.target as HTMLInputElement)?.value)"
+        :class="[{ 'outline-red-400!': props.error, 'pl-10': $slots.icon }, 's-input peer']"
+        :id="inputId"
+        :type="type"
+        :disabled="disabled"
+        :readonly="readonly"
+        :value="modelValue"
+      />
+
+      <!-- 👉 label -->
+      <label :for="inputId" v-if="props.label" class="s-input-label" :class="[labelClasses]">{{ props.label }}</label>
     </div>
-    <input
-      ref="inputEl"
-      v-bind="$attrs"
-      placeholder=" "
-      @[eventType]="$emit('update:modelValue', ($event.target as HTMLInputElement)?.value)"
-      class="grow bg-transparent border-none outline peer rounded-1 h-full w-full outline-gray-2 outline-1 px-4 transition-colors placeholder:text-sm focus:(outline-dark-600 outline-2)"
-      :class="[{ 'outline-red-400!': error, 'pl-10': $slots.icon }]"
-      :type="type"
-    />
-    <span
-      v-if="label"
-      class="w-max text-gray text-sm transition duration-300 pointer-events-none absolute"
-      :class="[
-        error && 'text-red-400!',
-        $slots.icon
-          ? 'peer-not-placeholder-shown:floating-label peer-focus:floating-label left-10'
-          : 'peer-not-placeholder-shown:(floating-label translate-x--4) peer-focus:(floating-label translate-x--4) left-4',
-      ]"
-      >{{ label }}</span
-    >
-  </label>
-  <p v-if="error" class="text-xs text-red-400">
-    {{ props.errorMessage }}
-  </p>
+
+    <!-- 👉 hint & error -->
+    <small v-if="props.error || props.hint" :class="[!props.error && 'text-gray-400', 'text-xs']">
+      {{ props.error ? props.errorMessage || props.hint : props.hint }}
+    </small>
+  </div>
 </template>
 
 <script lang="ts">
--- uno.config.ts
+++ uno.config.ts
@@ -3,25 +3,33 @@ import transformerVariantGroup from '@unocss/transformer-variant-group'
 import transformerDirectives from '@unocss/transformer-directives'
 
 export default defineConfig({
-  shortcuts: {
-    fcc: 'flex items-center justify-center',
-    column: 'flex flex-col items-center',
-    row: 'flex flex-row items-center',
-    'floating-label': 'scale-90 text-gray-900 translate-y--160% translate-x--4',
-    // form elements
-
-    // form-input
-
-    // toast
-    'sui-toast-layout': 'fixed left-50% top-70px -translate-x-50% fcc flex-col gap-4 z-9999 pointer-events-none',
-    'sui-toast': ' text-sm fcc gap-2 px-4 h-9 rounded-4px transition-all -translate-y-2.5 opacity-0',
-    'sui-toast-content': 'max-w-500px truncate',
-    'toast-warn': 'bg-yellow bg-opacity-10 text-yellow shadow',
-    'toast-success': 'bg-green bg-opacity-10 text-green shadow',
-    'toast-error': 'bg-red bg-opacity-10 text-red shadow',
-    'toast-info': 'bg-blue bg-opacity-10 text-blue shadow',
-    'toast-notification': 'bg-blue bg-opacity-10 text-blue shadow',
-  },
+  shortcuts: [
+    // common
+    {
+      center: 'flex items-center justify-center',
+      column: 'flex flex-col items-center',
+      row: 'flex flex-row items-center',
+      'floating-label': 'scale-80 text-gray-900 translate-y--160% translate-x--4',
+    },
+    // input-input
+    {
+      's-input':
+        'grow bg-transparent border-none outline rounded-1 h-10 w-full outline-gray-200 outline-1 read-only:(pointer-events-none outline-transparent bg-light-400) disabled:(cursor-not-allowed outline-transparent bg-light-900) px-4 transition-colors placeholder:text-sm focus:(outline-dark-600 outline-2) read-only:focus:(outline-gray-200 outline-1 cursor-default)',
+      's-input-label': 'w-max text-gray-400 text-sm transition duration-300 pointer-events-none absolute',
+      's-input-wrapper': '',
+    },
+    {
+      // toast
+      'sui-toast-layout': 'fixed left-50% top-70px -translate-x-50% center flex-col gap-4 z-9999 pointer-events-none',
+      'sui-toast': ' text-sm center gap-2 px-4 h-9 rounded-4px transition-all -translate-y-2.5 opacity-0',
+      'sui-toast-content': 'max-w-500px truncate',
+      'toast-warn': 'bg-yellow bg-opacity-10 text-yellow shadow',
+      'toast-success': 'bg-green bg-opacity-10 text-green shadow',
+      'toast-error': 'bg-red bg-opacity-10 text-red shadow',
+      'toast-info': 'bg-blue bg-opacity-10 text-blue shadow',
+      'toast-notification': 'bg-blue bg-opacity-10 text-blue shadow',
+    },
+  ],
   // ...
   transformers: [transformerVariantGroup(), transformerDirectives()],
 })
